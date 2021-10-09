71a8a36d86c90ec5ff316512aa179465fd411c4d
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2021-10-09 11:24:32 +0100

    tailwind installed and setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,7 +22,7 @@ const NavBar = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex lg:hidden md:hidden items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -31,16 +31,16 @@ const NavBar = () => {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="inset-y-0 left-0 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <NavLink
                     to="/"
-                    className="block xs:hidden h-8 w-auto text-blue-500 hover:bg-gray-700 hover:rounded text-lg font-medium"
+                    className="block xs:hidden h-8 w-auto text-blue-500 hover:bg-gray-700 hover:rounded-md  pl-20 text-lg font-medium"
                   >
                     Yenum Opone
                   </NavLink>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden lg:block sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <NavLink
