bb3c9b68b3c19645877e78c2de144d975742824d
officialyenum <oponechukwuyenum@gmail.com>
2023-01-23 11:15:44 +0100

    Bug: Fixed Rosary Error

--- src/pages/Rosary.jsx
+++ src/pages/Rosary.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 
 import '../Styles/Rosary.css';
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+// import Swal from "sweetalert2";
+// import withReactContent from "sweetalert2-react-content";
 
-const MySwal = withReactContent(Swal);
-const Toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener("mouseenter", Swal.stopTimer);
-    toast.addEventListener("mouseleave", Swal.resumeTimer);
-  },
-});
+// const MySwal = withReactContent(Swal);
+// const Toast = Swal.mixin({
+//   toast: true,
+//   position: "top-end",
+//   showConfirmButton: false,
+//   timer: 3000,
+//   timerProgressBar: true,
+//   didOpen: (toast) => {
+//     toast.addEventListener("mouseenter", Swal.stopTimer);
+//     toast.addEventListener("mouseleave", Swal.resumeTimer);
+//   },
+// });
 
 
 const Rosary = () => {
